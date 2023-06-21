44d115756852accb4a31119e2f09cc121d793b38
unknown <jonas.gastro91@gmail.com>
2023-06-21 12:53:58 -0300

    feat: reveal framer-motion working, saving Sample

--- src/pages/Services/styles.js
+++ src/pages/Services/styles.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 export const Container = styled.div`
   background: ${(props) => props.theme.bg.tertiary};
 
-  div{ 
-    background-color: black;
-    .sectionBox{
-      background-color: #fff;
-      height: 100vh;
-    }
+  .sectionBox{
+    background-color: #fff;
+    height: 100vh;
+    
+  }
+  .sectionBox2{
+    background-color: blueviolet;
+    height: 100vh;
   }
 `;
 
---
+++ src/utils/Reveal.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+
+export const Reveal = ({ children }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, {once: true});
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+
+    if (isInView){
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+
+  },[isInView]);
+
+  return (
+    <div ref={ref} style={{position: "relative", overflow:"hidden", width:"100%"}}>
+      <motion.div 
+        variants={{
+          hidden: { opacity: 0, y: 100 },
+          visible: { opacity: 1, y: 0 }
+        }}  
+        initial= "hidden"
+        animate= {mainControls}
+        transition={{
+          type:"spring",
+          bounce: 0.2,
+          duration: 3,
+          delay: 0.25
+        }}
+      >
+        {children}
+      </motion.div>
+      {/* <motion.div 
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" }
+        }}  
+        initial= "hidden"
+        animate= {slideControls}
+        transition={{
+          duration: 0.5,
+          easy: "easeIn"
+        }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          background: "var(--brand)",
+          zIndex: 20,
+        }}
+      >
+        {children}
+      </motion.div> */}
+    </div>
+  )
+};
