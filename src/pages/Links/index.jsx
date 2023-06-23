ce208abcc79314a3e7cae48f5a8eb4b0a955c64e
unknown <jonas.gastro91@gmail.com>
2023-06-23 12:11:30 -0300

    feat:utils links ok

--- src/pages/Links/index.jsx
+++ src/pages/Links/index.jsx
@@ -1,53 +1,100 @@
 import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { ThemeContext } from '../../context/ThemeContext';
+import { Reveal } from "../../utils/Reveal";
+import inss from "../../assets/links/inss.jpg";
+import mte from "../../assets/links/MTE.jpg";
+import notaFiscal from "../../assets/links/notaFiscal.jpg";
+import receitaFederal from "../../assets/links/receitaFederal.jpg";
+import receitaEstadual from "../../assets/links/receitaEstadual.jpg";
+import redeSim from "../../assets/links/rededim.jpg";
+import caixaEconomica from "../../assets/links/caixaEconomica.jpg";
+import SimplesNacional from "../../assets/links/SimplesNacional.jpg";
+import conselho from "../../assets/links/logo_cfc_vertical.jpg";
+import gov from "../../assets/links/logo.png";
+import tst from "../../assets/links/tst-tribunal.png";
+
 import * as Sc from './styles';
-import { motion } from 'framer-motion';
 
 export function Links() {
-  const navigate = useNavigate();
   const { theme } = useContext(ThemeContext);
-   
+
+  const images = [
+    {
+      name: 'inss',
+      src: inss,
+      site:"https://www.inss.gov.br/"
+    },
+    {
+      name: 'Ministério do Emprego',
+      src: mte,
+      site:"http://acesso.mte.gov.br"
+    },
+    {
+      name: 'Nota Fiscal',
+      src: notaFiscal,
+      site:"http://www.nfe.fazenda.gov.br"
+    },
+    {
+      name: 'Receita Federal',
+      src: receitaFederal,
+      site:"https://www.gov.br/receitafederal/pt-br"
+    },
+    {
+      name: 'Receita Estadual',
+      src: receitaEstadual,
+      site:"http://acesso.mte.gov.br"
+    },
+    {
+      name: 'Rede Sim',
+      src: redeSim,
+      site:"https://www.gov.br/empresas-e-negocios/pt-br/redesim"
+    },
+    {
+      name: 'Gov',
+      src: gov,
+      site:"https://www.gov.br/"
+    },
+    {
+      name: 'Caixa Economica',
+      src: caixaEconomica,
+      site:"https://www.caixa.gov.br/Paginas/home-caixa.aspx"
+    },
+    {
+      name: 'Simples Nacional',
+      src: SimplesNacional,
+      site:"https://www8.receita.fazenda.gov.br/SimplesNacional/"
+    },
+    {
+      name: 'Conselho Federal de Contabilidade',
+      src: conselho,
+      site:"https://cfc.org.br/"
+    },
+    {
+      name: 'Pesquisa de Jusisprudência',
+      src: tst,
+      site:"https://jurisprudencia.tst.jus.br/"
+    }
+  ];
+  
   return (
-    <motion.div 
-      initial={{x: 1000}}
-      animate={{x: 0}}
-      exit={{x: window.innerWidth, transition:{duration: 0.1}}}
-    >
-      <Sc.Container theme={theme}>
-        <div>
-          <h1> LINKS ÚTEIS </h1>
-          <h2>
-          http://acesso.mte.gov.br/data/files/FF8080812BCB2790012BCB6D37AD2F14/in_19891107_01.pdf</h2>
-
-          <h2>http://www.planalto.gov.br/ccivil_03/_ato2015-2018/2018/lei/l13709.htm</h2>
-
-          <h2>http://www.planalto.gov.br/ccivil_03/decreto-lei/del5452.htm</h2>
-
-          <h2>http://www.planalto.gov.br/ccivil_03/constituicao/constituicao.htm</h2>
-
-          <h2>https://jurisprudencia.tst.jus.br/?tipoJuris=SUM&orgao=TST&pesquisar=1</h2>
-
-          <h2>https://jurisprudencia.tst.jus.br/?tipoJuris=OJ&orgao=TST&pesquisar=1</h2>
-          
-          <h2>https://enquadramento.fecomercio.com.br/</h2>
-
-          <h2>https://in.gov.br/en/web/dou/-/portaria-359094139</h2>
-
-          <h2>https://www.gov.br/receitafederal/pt-br</h2>
-
-          <h2>https://www8.receita.fazenda.gov.br/SimplesNacional/</h2>
-
-          <h2>https://www.gov.br/empresas-e-negocios/pt-br/empreendedor</h2>
-
-          <h2>https://www.gov.br/empresas-e-negocios/pt-br/redesim</h2>
-
-          <h2>https://cfc.org.br/</h2>
-          
-          <h2>LINK RECEITA ESTADUAL</h2>
-          
-        </div>
-      </Sc.Container>
-    </motion.div>
+    <Sc.Container theme={theme} >
+      <div className="header">
+        <h1>Links uteis</h1>
+      </div>
+      <div className="body">
+        {images.map((image, index) => (
+          <div className="card" key={index}>
+            <Reveal>
+              <img src={image.src} alt={image.name} />
+            </Reveal>
+            <div className="titleCard">
+              <Reveal>
+                <span>{image.name}</span>
+              </Reveal>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Sc.Container>
   );
 }
--- src/pages/Links/styles.js
+++ src/pages/Links/styles.js
@@ -1,12 +1,41 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background: ${(props) => props.theme.bg.tertiary};
-  div{
-    padding-top: 8rem;
+  background: ${(props) => props.theme.bg.sideFilter};
+  padding-bottom: 15vh;
+  .header{
+    background: ${(props) => props.theme.bg.sideFilter};
+    color: ${(props) => props.theme.bg.sideFilter};
+  }
+  .body{
     display: flex;
-    flex-direction: column;
-    p{
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 3rem;
+    margin-top: 5rem;
+    .card{
+      width: 24vw;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      img{
+        width: 22vw;
+        margin-left: 1vw; 
+        background-color: #fff;
+        border-radius: 24px;
+      }
+      .titleCard{
+        text-align: center;
+        margin-top: 1rem;
+        span{
+          color: ${props => props.theme.color.text};
+          font-size: ${props => props.theme.sizeFonts.subtitle};
+          font-weight: ${props => props.theme.weightFonts.title1};
+          text-transform: uppercase;
+        }
+      }
     }
   }
 `;
